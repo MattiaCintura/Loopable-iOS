76479df73206f376e72335ddc2f7baa5fb32db56
Mattia Cintura <mattiacintura@icloud.com>
2023-03-31 14:28:32 +0200

    Initial Commit

---
+++ Loopable/LoopableApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopableApp.swift
+//  Loopable
+//
+//  Created by Mattia Cintura on 31/03/23.
+//
+
+import SwiftUI
+
+@main
+struct LoopableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
