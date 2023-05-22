66d39cdf22ae861af970565413f78811bbaf7890
Mattia Cintura <mattiacintura@icloud.com>
2023-05-22 20:17:43 +0200

    INCLUDE: Tests to SignUpWithEmailViewModel_Tests

--- Loopable_Tests/SignUpWithEmailViewModel_Tests.swift
+++ Loopable_Tests/SignUpWithEmailViewModel_Tests.swift
@@ -8,7 +8,44 @@
 import XCTest
 @testable import Loopable
 
+@MainActor
 final class SignUpWithEmailViewModel_Tests: XCTestCase {
 
+    func test_signUp_allPropertiesEmpty() async {
+        let vm = SignUpWithEmailViewModel()
 
+        do {
+            try await vm.signUp()
+        } catch {
+            XCTAssertEqual(error as? AuthenticationError, .requiredEmailAndPassword)
+        }
+    }
+
+    func test_signUp_invalidEmail() async {
+        let vm = SignUpWithEmailViewModel()
+        
+        vm.email = "example@loopable."
+        vm.password = "Test123"
+        vm.confirmPassword = "Test123"
+        
+        do {
+            try await vm.signUp()
+        } catch {
+            XCTAssertEqual(error as? AuthenticationError, .invalidEmail)
+        }
+    }
+    
+    func test_signUp_confirmPasswordNotEqual() async {
+        let vm = SignUpWithEmailViewModel()
+        
+        vm.email = "example@loopable.it"
+        vm.password = "Test123"
+        vm.confirmPassword = "Test1234"
+        
+        do {
+            try await vm.signUp()
+        } catch {
+            XCTAssertEqual(error as? AuthenticationError, .confirmPassword)
+        }
+    }
 }
